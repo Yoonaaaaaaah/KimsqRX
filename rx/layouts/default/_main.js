c4edf9acf8f163dc3d6cf827001e0c30633a642e
Yoonaaaaaaah <yoonaaaaaaah@gmail.com>
2012-09-18 10:07:52 +0900

    rx_1.1.0_20120918

---
+++ rx/layouts/default/_main.js
@@ -0,0 +1,10 @@
+﻿function showM(m)
+{
+	var box = getId('subMenuBox'+m);
+	if(box) box.style.display = 'block';
+}
+function hideM(m)
+{
+	var box = getId('subMenuBox'+m);
+	if(box) box.style.display = 'none';
+}
